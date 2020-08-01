9afff6368e17e846b1a9575693340bff51ce438b
nvm <nevermn@protonmail.com>
2020-08-01 13:28:14 +0300

    Add write json utility method

--- web/server.go
+++ web/server.go
@@ -1,8 +1,10 @@
 package web
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -15,6 +17,7 @@ type ctxKey int
 
 const (
 	keyDb ctxKey = iota
+	keyProjectId
 )
 
 func StartServer() {
@@ -32,6 +35,7 @@ func StartServer() {
 		r.Post("/", createProject)
 
 		r.Route("/{projectID}", func(r chi.Router) {
+			r.Use(intURIParamMiddleware("projectID", keyProjectId))
 			r.Get("/", getProject)
 			r.Get("/columns", getColumns)
 			r.Put("/", updateProject)
@@ -48,12 +52,22 @@ func StartServer() {
 			r.Delete("/", deleteColumn)
 		})
 	})
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("chiii"))
-	})
 	http.ListenAndServe(":3000", r)
+}
 
+func intURIParamMiddleware(paramName string, key interface{}) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			val := chi.URLParam(r, paramName)
+			intval, err := strconv.Atoi(val)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("%s must be an int", paramName), http.StatusBadRequest)
+				return
+			}
+			r = r.WithContext(context.WithValue(r.Context(), key, intval))
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
 func listProjects(w http.ResponseWriter, r *http.Request) {
@@ -63,33 +77,18 @@ func listProjects(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
-	response, err := json.Marshal(projects)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	w.Write(response)
+	writeJson(w, projects)
 }
 
 func getProject(w http.ResponseWriter, r *http.Request) {
-	projectID := chi.URLParam(r, "projectID")
-	ID, err := strconv.Atoi(projectID)
-	if err != nil {
-		http.Error(w, "project id must be an int", http.StatusBadRequest)
-		return
-	}
+	projectID := r.Context().Value(keyProjectId).(int)
 	db := r.Context().Value(keyDb).(db.Database)
-	project, err := db.GetProject(ID)
+	project, err := db.GetProject(projectID)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("project %d not found", ID), http.StatusNotFound)
+		http.Error(w, fmt.Sprintf("project %d not found", projectID), http.StatusNotFound)
 		return
 	}
-	response, err := json.Marshal(project)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	w.Write(response)
+	writeJson(w, project)
 }
 
 func createProject(w http.ResponseWriter, r *http.Request) {
@@ -122,4 +121,13 @@ func deleteColumn(w http.ResponseWriter, r *http.Request) {
 
 func swapColumns(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func writeJson(w http.ResponseWriter, val interface{}) {
+	marshaled, err := json.Marshal(val)
+	if err != nil {
+		log.Printf("Error encoding to json %v\n", marshaled)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+	w.Write(marshaled)
 }
