1a5d53bce10509d0c22f6d7ff0712585ec4e2ff4
nvm <nevermn@protonmail.com>
2020-08-02 20:24:18 +0300

    Init and insert projects and columns

--- db/postgres.go
+++ db/postgres.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"time"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
@@ -15,10 +14,10 @@ func Run() {
 	if err != nil {
 		log.Fatal("failed to connect to db", err.Error())
 	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("failed to connect to db: ping")
-	}
+	// err = db.Ping()
+	// if err != nil {
+	// 	log.Fatal("failed to connect to db: ping")
+	// }
 
 	err = Migrate(db)
 	if err != nil {
@@ -27,74 +26,118 @@ func Run() {
 
 	err = InsertThings(db)
 	if err != nil {
-		log.Fatal("failed to insert things", err.Error())
+		log.Fatal("failed to create projects: ", err.Error())
 	}
 
-	things, err := GetThings(db)
-	if err != nil {
-		log.Fatal("failed to get things", err.Error())
-	}
+	// things, err := GetThings(db)
+	// if err != nil {
+	// 	log.Fatal("failed to get things", err.Error())
+	// }
 
-	for _, thing := range things {
-		log.Printf("%v\n", thing)
-	}
+	// for _, thing := range things {
+	// 	log.Printf("%v\n", thing)
+	// }
 
 }
 
-type Thing struct {
-	ID      int
-	Name    string
-	Created time.Time
+type Project struct {
+	ID          int
+	Columns     []*Column
+	Name        string
+	Description string
+}
+
+type Column struct {
+	Position int
+	Project  *Project `json:"-"`
+	Name     string
 }
 
 func Migrate(db *sql.DB) error {
-	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS Test (
-                       id serial PRIMARY KEY,
-                       name varchar(20) NOT NULL,
-                       created timestamp
-                       );
-               `)
+	_, err := db.Exec(
+		`CREATE TABLE IF NOT EXISTS Project (
+		id serial PRIMARY KEY,
+		name varchar(20) NOT NULL,
+		description text NOT NULL,
+        created timestamp
+        );
+	`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS Col (
+		id serial PRIMARY KEY,
+		project_id INT,
+		position smallint NOT NULL,
+		name varchar(20) NOT NULL,
+		created timestamp,
+		CONSTRAINT fk_project
+			FOREIGN KEY(project_id)
+			REFERENCES Project(id)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+        );
+	`)
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
 func InsertThings(db *sql.DB) error {
-	stmt, err := db.Prepare("INSERT INTO test (name, created) VALUES($1, NOW())")
+	stmtProj, err := db.Prepare("INSERT INTO Project (name, description, created) VALUES($1, $2, NOW()) RETURNING id")
 	if err != nil {
 		return err
 	}
-	defer stmt.Close()
+	stmtCol, err := db.Prepare("INSERT INTO Col (project_id, position, name, created) VALUES($1, $2, $3, NOW())")
+	if err != nil {
+		return err
+	}
+	defer stmtProj.Close()
+	defer stmtCol.Close()
+	var lastInsertedID int
 	for i := 0; i < 10; i++ {
-		_, err = stmt.Exec(fmt.Sprintf("thing %d", i))
+		err := stmtProj.QueryRow(fmt.Sprintf("name %d", i), "").Scan(&lastInsertedID)
 		if err != nil {
 			return err
 		}
+		for j := 0; j < 3; j++ {
+			_, err := stmtCol.Exec(lastInsertedID, j, fmt.Sprintf("col name %d", i*10+j))
+			if err != nil {
+				return err
+			}
+		}
+
 	}
 	return nil
 }
 
-func GetThings(db *sql.DB) ([]*Thing, error) {
-	rows, err := db.Query("SELECT * FROM test")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	var (
-		id      int
-		name    string
-		created time.Time
-	)
-	things := make([]*Thing, 0)
-	for rows.Next() {
-		err := rows.Scan(&id, &name, &created)
-		if err != nil {
-			return nil, err
-		}
-		thing := &Thing{ID: id, Name: name, Created: created}
-		things = append(things, thing)
-	}
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
-	return things, nil
-}
+// func GetThings(db *sql.DB) ([]*Thing, error) {
+// 	rows, err := db.Query("SELECT * FROM test")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer rows.Close()
+// 	var (
+// 		id      int
+// 		name    string
+// 		created time.Time
+// 	)
+// 	things := make([]*Thing, 0)
+// 	for rows.Next() {
+// 		err := rows.Scan(&id, &name, &created)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		thing := &Thing{ID: id, Name: name, Created: created}
+// 		things = append(things, thing)
+// 	}
+// 	err = rows.Err()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return things, nil
+// }
